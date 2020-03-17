39eb42a9387dbc598b438b448e1de266ffff81ee
rushi444 <rushi44@gmail.com>
2020-03-17 12:46:53 -0700

    fixed dependencies

--- server/server.js
+++ server/server.js
@@ -8,13 +8,13 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://rushi44:Password123@cluster0-adwdx.mongodb.net/test?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
