2fa6cec5362b9475e4bb5527463e496a2de9a55e
rushi444 <rushi44@gmail.com>
2020-03-19 04:29:24 -0700

    first graphql call :)

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-export const SongList = () => {
-  return <div>SongList</div>;
+const SongList = ({ data }) => {
+  const renderSongs = () => {
+    return data.songs.map(song => (
+      <li key={song.id} className='collection-item'>
+        {song.title}
+      </li>
+    ));
+  };
+
+  return (
+    <ul className='collection'>
+      {data.loading ? <div>Loading</div> : renderSongs()}
+    </ul>
+  );
 };
+
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
-import { SongList } from './components/SongList';
+import  SongList  from './components/SongList';
 
 const client = new ApolloClient({});
 
